b0470b70874c58bcc5436e6029fffcdfb652b65c
Diogo Rodrigues <diogo.rosinha.rodrigues@tecnico.ulisboa>
2023-02-07 17:27:45 +0000

    final

--- software/iob-gpio.h
+++ software/iob-gpio.h
@@ -7,15 +7,6 @@
 //Set GPIO base address
 void gpio_init(int base_address);
 
-//Get values from inputs
-uint32_t gpio_get();
-
-//Set values on outputs
-void gpio_set(uint32_t outputs);
-
-//Set mask for outputs (bits 1 are driven outputs, bits 0 are tristate)
-void gpio_set_output_enable(uint32_t value);
-
 uint16_t gpio_sw_get();
 
 //Set mask for outputs (bits 1 are driven outputs, bits 0 are tristate)
